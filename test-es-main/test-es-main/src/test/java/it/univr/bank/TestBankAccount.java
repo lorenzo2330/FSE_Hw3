d4637364ec79fd02e00110c5c494140ecb26738c
lorenzo2330 <lorenzo.dambro@gmail.com>
2026-02-08 09:36:24 +0100

    Add files via upload

---
+++ test-es-main/test-es-main/src/test/java/it/univr/bank/TestBankAccount.java
@@ -0,0 +1,14 @@
+package it.univr.bank;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class TestBankAccount {
+
+    @Test
+    public void testEmpty(){BankAccount b = new BankAccount();
+    }
+
+
+}
